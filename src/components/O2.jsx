2a3cf1558761e26dad9f5a2487e20eb23779fab2
Dominik Lewicki <dominik_03-06@wp.pl>
2024-02-08 16:37:09 +0100

    new123

--- src/components/O2.jsx
+++ src/components/O2.jsx
@@ -20,25 +20,25 @@ function O2() {
     className='h-[12dvh] w-full mx-auto max-w-[1100px] pl-32 pr-32 flex justify-center exsm:pl-10 exsm:pr-10'>
       <div className='w-[100%] h-[12dvh] flex justify-between items-center max-w-[1100px] exsm:flex-col md:flex-row exsm:w-[100%]'>
         <div className='flex w-1/2 exsm:w-full exsm:h-1/2 sm:bg-transparent'>
-          <div className='w-[33%] h-full flex items-center justify-center'>
-            <img className='w-3/4 h-1/2' src={Icon1} loading='lazy' alt='Icon 1' />
+          <div className='w-[33%] h-full flex items-center justify-center '>
+            <img className='w-3/4 h-[30px]' src={Icon1} loading='lazy' alt='Icon 1' />
+          </div>
+          <div className='w-[33%] h-full flex items-center justify-center '>
+            <img className='w-3/4 h-[30px]' src={Icon2} loading='lazy' alt='Icon 2' />
           </div>
           <div className='w-[33%] h-full flex items-center justify-center'>
-            <img className='w-3/4 h-1/2' src={Icon2} loading='lazy' alt='Icon 2' />
-          </div>
-          <div className='w-[33%] h-full flex items-center justify-center'>
-            <img className='w-3/4 h-1/2' src={Icon3} loading='lazy' alt='Icon 3' />
+            <img className='w-3/4 h-[30px]' src={Icon3} loading='lazy' alt='Icon 3' />
           </div>
         </div>
         <div className='flex h-20 w-1/2 exsm:w-full exsm:h-1/2'>
           <div className='w-[33%] h-full flex items-center justify-center '>
-            <img className='w-3/4 h-1/2' src={Icon4} loading='lazy' alt='Icon 4' />
+            <img className='w-3/4 h-[30px]' src={Icon4} loading='lazy' alt='Icon 4' />
           </div>
           <div className='w-[33%] h-full flex items-center justify-center'>
-            <img className='w-1/2 h-1/2' src={Icon5} loading='lazy' alt='Icon 5' />
+            <img className='w-1/2 h-[30px]' src={Icon5} loading='lazy' alt='Icon 5' />
           </div>
           <div className='w-[33%] h-full flex items-center justify-center'>
-            <img className='w-1/2 h-1/2' src={Icon6} loading='lazy' alt='Icon 6' />
+            <img className='w-1/2 h-[30px]' src={Icon6} loading='lazy' alt='Icon 6' />
           </div>
         </div>
       </div>
