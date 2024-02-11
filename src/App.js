7809bdaa0fe2cc193f9362c3684274cae3c5df9d
Dominik Lewicki <dominik_03-06@wp.pl>
2024-02-11 17:21:06 +0100

    lk

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import O1 from "./components/O1"; 
 import O2 from "./components/O2"; 
-import O3 from "./components/O3"; 
 import O4 from "./components/O4"; 
 import O5 from "./components/O5"; 
 import "./App.css";
@@ -15,7 +14,7 @@ function App() {
     <Navbar/>
       <O1 />
       <O2 />
-      <O3 />
+   
       <O4 />
       <O5 />
       </body>
