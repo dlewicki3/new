b4d7fa1db50186c207c80750d5eaf253682313e3
Dominik Lewicki <dominik_03-06@wp.pl>
2024-02-24 23:08:54 +0100

    ppp

--- src/components/O4.jsx
+++ src/components/O4.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import './O4.css'; // Import stylów CSS
+import I1 from './I1.webp';
+import I2 from './I2.webp';
+import I3 from './I3.jpeg';
+import I4 from './I4.webp';
+import { MdClose } from "react-icons/md";
+
 
 function O4() {
   // Stan do śledzenia otwarcia modala i widoczności chuj2, chuj3 i chuj4
@@ -44,17 +50,37 @@ function O4() {
         {/* Górna część modala */}
         <div className="gora44">
           {/* Kwadrat - główny przycisk otwierający modal */}
-          <motion.div className="square1" onClick={handleAnimationButtonClick}>
-            chuj
+          
+          <motion.div className="square1" onClick={handleAnimationButtonClick}  >
+            <div className="na-foto4">
+              <img className='foto4' src= {I1}></img>
+            </div>
           </motion.div>
+
+
           {/* Drugi kwadrat - przycisk otwierający chuj2 */}
-          <motion.div className="kwa44" onClick={handleChuj2Click}></motion.div>
+          <motion.div className="kwa44" onClick={handleChuj2Click}>
+          <div className="na-foto4">
+              <img className='foto4' src= {I2}></img>
+            </div>
+
+          </motion.div>
         </div>
         {/* Dolna część modala */}
         <div className="dol44">
           {/* Elementy dolnej części */}
-          <motion.div className="kwa44-dol" onClick={handleChuj3Click}></motion.div>
-          <motion.div className="kwa44-dol" onClick={handleChuj4Click}></motion.div>
+          <motion.div className="kwa44-dol" onClick={handleChuj3Click}>
+        
+          <div className="na-foto4">
+           
+              <img className='foto4' src= {I3}></img>
+            </div>
+          </motion.div>
+          <motion.div className="kwa44-dol2" onClick={handleChuj4Click}>
+          <div className="na-foto4">
+              <img className='foto4' src= {I4}></img>
+            </div>
+          </motion.div>
         </div>
       </div>
 
@@ -62,7 +88,14 @@ function O4() {
       {isChuj2Visible && (
         <div className="modal-bg">
           <motion.div className="chuj2">
-            <button className='end-btn' onClick={handleCloseButtonClick}>Zamknij</button>
+            <div className="top4">
+              <img className='foto44'src= {I2}>
+
+              </img>
+            </div>
+            <button className='end-btn' onClick={handleCloseButtonClick}>
+            <MdClose/>
+            </button>
           </motion.div>
         </div>
       )}
@@ -71,7 +104,9 @@ function O4() {
       {isChuj3Visible && (
         <div className="modal-bg">
           <motion.div className="chuj3">
-            <button className='end-btn' onClick={handleCloseButtonClick}>Zamknij</button>
+            <button className='end-btn' onClick={handleCloseButtonClick}>
+            <MdClose/>
+            </button>
           </motion.div>
         </div>
       )}
@@ -80,7 +115,9 @@ function O4() {
       {isChuj4Visible && (
         <div className="modal-bg">
           <motion.div className="chuj4">
-            <button className='end-btn' onClick={handleCloseButtonClick}>Zamknij</button>
+            <button className='end-btn' onClick={handleCloseButtonClick}>
+            <MdClose/>
+            </button>
           </motion.div>
         </div>
       )}
@@ -89,7 +126,14 @@ function O4() {
       {isOpen && (
         <div className="modal-bg">
           <motion.div className="big-square">
-            <button className='end-btn' onClick={handleCloseButtonClick}>Zamknij</button>
+          <div className="top4">
+              <img className='foto44'src= {I2}>
+
+              </img>
+            </div>
+            <button className='end-btn' onClick={handleCloseButtonClick}> 
+            <MdClose/>
+            </button>
           </motion.div>
         </div>
       )}
