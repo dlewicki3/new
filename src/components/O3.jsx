d7bbbc0d16d3e4c212d8b16e65c49a2903545aaf
Dominik Lewicki <dominik_03-06@wp.pl>
2024-02-24 17:30:45 +0100

    fff

--- src/components/O3.jsx
+++ src/components/O3.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import sand from './sand.webp';
-
+import './O3.css';
 function O3() {
   return (
-    <div className='lg:h-[65dvh] mx-auto max-w-[1100px]  xl:h-[65dvh] flex items-center sm:h-[50dvh]fck:h-[80dvh] exsm:h-[50dvh]  fck:pl-0  '>
+    <div className='o3'>
 
 
 
-      <div className='w-[1100px]  h-[180px]  flex justify-center md:pl-20 md:pr-20md:bg-transparent sm:pl-10 sm:pr-10 exsm:pl-5 exsm:pr-5 lg:h-[60dvh]  exsm:w-[100%] md:h-[40dvh] sm:h-[30vh] '>
-        <img className='max-w-full h-auto sm:h-[100%] rounded-[12px]  ' src={sand} alt="Piasek"></img>
+      <div className='k3 '>
+         <img className=' foto ' src={sand} alt="Piasek"></img> 
       </div>
 
 
