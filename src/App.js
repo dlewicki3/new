ca39f52af0042c094f10275d7ac785ff2ccc101f
Dominik Lewicki <dominik_03-06@wp.pl>
2024-02-29 23:04:31 +0100

    ddddddddddd

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Navbar from "./components/Navbar";
 import O1 from "./components/O1"; 
 import O2 from "./components/O2"; 
 import O3 from "./components/O3"; 
-
+import O4 from "./components/O4"; 
 import O5 from "./components/O5"; 
+import O6 from "./components/O6"; 
 
 
 import "./App.css";
@@ -18,8 +19,9 @@ function App() {
       <O1 />
       <O2 />
       <O3 />
-   
+      <O4 />
       <O5 />
+      <O6 />
 
    
       </body>
---
+++ src/components/O6.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./O6.css";
+
+function O6() {
+  return (
+<div className="o6">
+    <div className="k6">
+
+    </div>
+</div>
+  )
+}
+
+export default O6
